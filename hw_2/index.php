a081f0ba716ac0aada125120964690f2ce02228a
Danya Gichak <wemlirz35@gmail.com>
2024-12-16 13:57:37 +0200

    commit

--- hw_2/index.php
+++ hw_2/index.php
@@ -8,10 +8,10 @@ function renderCircles($count = 10) {
     echo "</div>";
 }
 renderCircles();
+//----------4
 function binaryToHex($binary) {
     $decimal = bindec($binary);
     $hex = dechex($decimal);
     return $hex;
 }
 echo "<p>Number 1010 in hex: " . binaryToHex("1010") . "</p>";
-//----------4
